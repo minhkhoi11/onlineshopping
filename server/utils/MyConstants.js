2ea2beddcb951a2f5db0d7e26d2e6e8da436c64f
minhkhoi11 <151599556+minhkhoi11@users.noreply.github.com>
2023-11-27 17:01:33 +0700

    server

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,12 @@
+const MyConstants = {
+  DB_SERVER: 'cluster.rnszsus.mongodb.net',
+  DB_USER: 'minhkhoi',
+  DB_PASS: 'minhkhoi789',
+  DB_DATABASE: 'shoppingonline',
+  JWT_SECRET: '0932709905',
+  JWT_EXPIRES: '1000000000000000', // in milliseconds
+  EMAIL_USER: 'khoiminh6785@gmail.com', // gmail service
+  EMAIL_PASS: 'yjvt uxfn jjvh qzlw'
+  
+};
+module.exports = MyConstants;
